a88ed094bd5446f2e454905450d0c27d4f722b43
pmarshall111 <pmarshall.dev@gmail.com>
2021-10-04 20:40:00 +0100

    Removing DbTables dependency

--- src/main/java/com/football/betting/api/datasource/repository/BetRepository.java
+++ src/main/java/com/football/betting/api/datasource/repository/BetRepository.java
@@ -1,16 +1,8 @@
 package com.football.betting.api.datasource.repository;
 
 import com.football.betting.api.datasource.entity.BetEntity;
-import dbTables.BetTable;
-import dbTables.MatchTable;
-import dbTables.PredictionTable;
-import dbTables.TeamTable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface BetRepository extends CrudRepository<BetEntity, Integer> {
--- src/main/java/com/football/betting/api/datasource/repository/PredictionRepository.java
+++ src/main/java/com/football/betting/api/datasource/repository/PredictionRepository.java
@@ -1,15 +1,8 @@
 package com.football.betting.api.datasource.repository;
 
 import com.football.betting.api.datasource.entity.PredictionEntity;
-import dbTables.MatchTable;
-import dbTables.PredictionTable;
-import dbTables.TeamTable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface PredictionRepository extends CrudRepository<PredictionEntity, Integer> {
