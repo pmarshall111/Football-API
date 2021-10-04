00ddfac65052b37eaa91e50c1c9a6e9a8fc4f5e1
pmarshall111 <pmarshall.dev@gmail.com>
2021-10-04 20:16:08 +0100

    FP-5 Adding GitHub Actions workflow and updating JDBC driver to a MariaDB driver

--- src/main/java/com/football/betting/api/FootballBettingApiApplication.java
+++ src/main/java/com/football/betting/api/FootballBettingApiApplication.java
@@ -2,12 +2,11 @@ package com.football.betting.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FootballBettingApiApplication {
-
+public class FootballBettingApiApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(FootballBettingApiApplication.class, args);
 	}
-
 }
